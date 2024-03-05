db86993dac32ebfc659c0f9d6fbd390c50215faa
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2024-03-05 12:43:29 +0200

    Update genezio delete script
    Signed-off-by: Andreia Ocanoaia <andreia.ocanoaia@gmail.com>

--- delete_all_projects.sh
+++ delete_all_projects.sh
@@ -2,17 +2,22 @@
 
 # This script deletes all projects in Genezio
 
-# Test of env variable $GENEZIO_TOKEN is set
-if [ -z ${var+x} ]; then
-    echo GENEZIO_TOKEN is set
-else
-    echo GENEZIO_TOKEN is not set. We are setting it for you
-    export GENEZIO_TOKEN=$(cat ~/.geneziorc)
-fi
+projects=$(genezio list -l |  tr -d " \t," | grep "ID:" | cut -d ":" -f2)
+projects_to_skip=("36b78717-d7cf-48b0-9b51-68d88ef4273d" "8d179259-fda1-4a15-a473-61fd9547777f" "21696c7c-c099-4098-8648-f731fcb7daf5")
 
-projects=$(genezio ls -l |  tr -d " \t," | grep "ID:" | cut -d ":" -f2)
+counter=0
+skip_count=1
 
 for project in $projects
 do
-    genezio delete --force $project
+    if [[ " ${projects_to_skip[@]} " =~ " ${project} " ]]; then
+        echo "Skipping project: $project"
+        continue
+    fi
+    if [ $counter -ge $skip_count ]; then
+        echo "Deleting project: $project"
+        genezio delete --force "$project"
+    fi
+
+    ((counter++))
 done
