3810ce3c4b007bab2351c73102e8a80ab8ccf9e7
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2025-05-18 18:12:55 +0300

    Improvements
    Signed-off-by: Andreia Ocanoaia <andreia.ocanoaia@gmail.com>

--- delete_all_project_confirmation.sh
+++ delete_all_project_confirmation.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <GENEZIO_TOKEN>"
+if [ -z "$2" ]; then
+    echo "Usage: $0 <ENVIRONMENT> <GENEZIO_TOKEN>"
     exit 1
 fi
 
-token=$1
+environment=$1
+token=$2
 
-response=$(curl -s -H "Authorization: Bearer $token" "https://api.genez.io/users/user")
+if [[ "$environment" == "dev" ]]; then
+    BACKEND_URL="https://dev.api.genez.io"
+elif [[ "$environment" == "prod" ]]; then
+    BACKEND_URL="https://api.genez.io"
+else
+    echo "Invalid environment. Use 'dev' or 'prod'."
+    exit 1
+fi
+
+response=$(curl -s -H "Authorization: Bearer $token" "$BACKEND_URL/users/user")
 email=$(echo "$response" | jq -r '.user.email')
 subscription_plan=$(echo "$response" | jq -r '.user.subscriptionPlan')
 
@@ -20,14 +30,14 @@ if [[ "$confirm" != "y" ]]; then
     echo "Aborting."
     exit 1
 fi
-echo "Proceeding with the deletion of all projects."
+echo "Proceeding with the deletion..."
 
 start_index=0
 limit=100
 # Make the API request to list projects
 projects=$(curl -s -H "Authorization: Bearer $token" \
                   -H "Accept-Version: genezio-cli/3.0.7" \
-                  "https://api.genez.io/projects?startIndex=$start_index&projectsLimit=$limit")
+                  "$BACKEND_URL/projects?startIndex=$start_index&projectsLimit=$limit")
 
 project_ids=($(echo "$projects" | jq -r '.projects[].id'))
 project_names=($(echo "$projects" | jq -r '.projects[].name'))
@@ -41,7 +51,7 @@ for i in "${!project_ids[@]}"; do
 
     if [[ "$confirm" == "y" ]]; then
         echo "Deleting $project_name..."
-        # GENEZIO_TOKEN="$token" genezio delete --force "$project_id"
+        GENEZIO_TOKEN="$token" genezio delete --force "$project_id" > /dev/null
         echo "$project_name deleted successfully."
     else
         echo "Skipped deleting $project_name."
